f0dc065ca864c700e665e963c10cc6b3c1ff84d7
Juhwan Kim <juhwan.boxcat@gmail.com>
2022-11-28 14:06:19 +0900

    사탕을 플레이어가 먹으면 사탕이 사라지도록 CandyScript 추가함.

---
+++ Assets/Scripts/CandyScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CandyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            //Increment score
+            Destroy(gameObject);
+        }
+    }
+}
